4e03fd70118ca9b75684402e76c29afac6d22b2f
Jeremy Hummel <jehummel@cisco.com>
2012-07-16 16:28:22 -0700

    Passes test suite

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,6 +38,8 @@ describe User do
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
   it { should respond_to(:unfollow!) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -177,6 +179,11 @@ describe User do
 
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
   end
 end
 
