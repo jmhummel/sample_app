6c22255f7578f622a7c047bbaec6e3aac6ea2c03
Jeremy Hummel <jehummel@cisco.com>
2012-07-18 10:50:30 -0700

    Tests pass - 11.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,6 +51,10 @@ class User < ActiveRecord::Base
     relationships.find_by_followed_id(other_user.id).destroy
   end
 
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
+
   private
 
     def create_remember_token
